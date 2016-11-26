a92967b786de836c4af9cb9a7e206006fbb782c6
Andrey Kostylev <kostylevandrey113@gmail.com>
2016-11-26 12:11:00 +0300

    qwe

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "h16"
+    name: "h16",
+    dependencies: [
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-WebSockets.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0)
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+import PerfectLib
+import PerfectWebSockets
+import PerfectHTTP
+import PerfectHTTPServer
+
+let server = HTTPServer()
+var routes = Routes()
+
+
+configureServer(server)
+
+// Static
+
+let documentRoot = "./Web"
+
+let staticFileHandler = StaticFileHandler(documentRoot: documentRoot)
+
+
+routes.add(method: .get, uri: "*", handler: { request, response in
+    staticFileHandler.handleRequest(request: request, response: response)
+})
+
+do {
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
+
